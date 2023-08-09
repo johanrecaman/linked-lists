6e147363ad6fa6433eb85f98ef276d5b281cde07
Johan Stromberg Recaman <logzinr6@gmail.com>
2023-08-08 23:19:06 -0300

    linked lists exercises finished

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,6 +1,7 @@
 require_relative 'node'
 
 class LinkedList
+    attr_accessor :head, :tail
     def initialize
         @head = nil
         @tail = nil
@@ -27,10 +28,70 @@ class LinkedList
         end
     end
 
-    def printList
-        while @head
-            print "(#{@head.value}) -> "
-            @head = @head.next_node
+    def size
+        count = 0
+        node = @head
+        while node
+            count += 1
+            node = node.next_node
+        end
+        count
+    end
+
+    def head
+        @head.value
+    end
+
+    def tail
+        @tail.value
+    end
+
+    def at(index)
+        node = @head
+        for i in 0..index
+            node = node.next_node
+        end
+        node
+    end
+    
+    def pop
+        node = @head
+        while node.next_node.next_node
+            node = node.next_node
+        end
+        node.next_node = nil
+        @tail = node
+    end
+
+    def contains?(value)
+        node = @head
+        while node
+            if node.value == value
+                return true
+            end
+            node = node.next_node
+        end
+        false
+    end
+
+    def find(value)
+        node = @head
+        count = 1
+        while node
+            if node.value == value
+                return count
+            end
+            node = node.next_node
+            count += 1
+        end
+        nil
+    end
+
+    def to_s
+        node = @head
+        while node
+            print "(#{node.value}) -> "
+            node = node.next_node
         end
         puts "nil"
     end
--- main.rb
+++ main.rb
@@ -1,7 +1,27 @@
 require_relative 'lib/linked_list'
 
 list = LinkedList.new
+
+#test it out
 list.append(1)
 list.append(2)
+list.append(3)
+list.append(4)
+list.append(5)
+
 list.prepend(0)
-list.printList
+list.prepend(-1)
+list.prepend(-2)
+
+puts list.size
+
+puts list.head
+puts list.tail
+
+puts list.at(3).value
+puts list.pop.value
+
+puts list.contains?(3)
+puts list.find(3)
+
+puts list.to_s
